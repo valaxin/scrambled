52a89c9cf08cf3106a71228e329d362697695550
MacKenzie Bobbitt <mackenzie.bobbitt@outlook.com>
2020-06-14 22:44:55 -0300

    (semi)working set command, refinement needed

--- commands/set.js
+++ commands/set.js
@@ -1,13 +1,59 @@
 const fs = require('fs')
+const path = require('path')
+
+const updateConfig = async (message, key, value) => {
+	let location = path.resolve(__dirname, '../config.json')
+	let config = require(location)
+
+	// does the key already exist within the document?
+	let existingKey = Object.keys(config).map(configKey => {
+		return configKey === key
+	})
+
+	if (existingKey) {
+		let question = `**Warning!** you're about to overwrite property \`${key}\`, continue? Y/N`
+		message.reply(question)
+
+		try {
+      let waitOptions = { max: 1, time: 30000 }
+      let waitForResponse = await message.channel.awaitMessages(m => m.author.id == message.author.id, waitOptions)
+			if (waitForResponse.first().content.match(/^n(o)?$/gi) !== null) {
+				// abandon ship!
+				message.reply(`**Abandoned!**, try again with a different key name`)
+				return
+			}
+			
+			config[key] = value
+			await fs.writeFile(location, JSON.stringify(config), 'utf8', (err) => {
+				if (err) {
+					console.log(err)
+					return
+				}
+			})
+			message.reply(`**Huzzah!** successfully updated property`)
+			
+
+    } catch (err) {
+			message.reply(`**Timeout!** no selection was made`)
+			return
+    }
+	} else {
+		message.reply(`***Uh-Oh!***`)
+	}
+
+}
 
 module.exports = {
 	name: 'set',
 	description: 'set differnt values for bot',
-	status: ':red_square:',
-	execute(message, options) {
+	status: ':yellow_square:',
+	async execute(message, options) {
 		// permission?
-		// switch/case on option[0]
+		// switch/case on option[0] ? no.
 		// cases being? prefix, default alert channel, users (twitch|mixer), MAYBE presence
 		// these should all be behind owner/admin roles
-	},
+
+		if (options.length < 1) return
+		await updateConfig(message, options[0], options[1])
+	}
 }
--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const Discord = require('discord.js')
 const Client = require('./client/Client')
 const config = require('./config.json')
 
-const client = new Client()
-
-client.commands = new Discord.Collection()
-
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+const client = new Client()
+client.commands = new Discord.Collection()
 
 commandFiles.forEach(file => {
 	const command = require(`./commands/${file}`)
@@ -15,28 +14,22 @@ commandFiles.forEach(file => {
 })
 
 client.on('ready', async () => {
-
 	await Promise.all(config.users.mixer.map(user => {	
 		return client.commands.get('mixer').execute(false, [user, '.5', config.keys.discord.defaultChannel], client)
 	}))
-
 	await Promise.all(config.users.twitch.map(user => {	
 		return client.commands.get('twitch').execute(false, [user, '.5', config.keys.discord.defaultChannel], client, config.keys.twitch.id)
 	}))
-
 })
 
 client.on('reconnecting', () => {	console.log('Reconnecting!') })
 client.on('disconnect', () => { console.log('Disconnect!') })
 
 client.on('message', async message => {
+	if (message.author.bot) return
+	if (!message.content.startsWith(config.prefix)) return
 	let options = message.content.slice(config.prefix.length).split(/ +/)
 	let command = client.commands.get(options.shift().toLowerCase())
-	
-	if (message.author.bot) return
-	
-	if (!message.content.startsWith(config.prefix)) return
-	
 	try {
 		command.execute(message, options, client)
 	} catch (error) {
