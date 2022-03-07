c2ffd6833812068ee154350d881a64ee5ca925b0
MacKenzie <mackenzie.bobbitt@outlook.com>
2022-03-07 15:27:44 -0400

    formatting fixes...

--- commands/anime.js
+++ commands/anime.js
@@ -1,84 +1,85 @@
 import fetch from 'node-fetch'
-import { Interaction } from 'discord.js'
+import {
+	Interaction
+} from 'discord.js'
 
 const APIBase = 'https://lite-api.animemate.xyz'
 
-async function embed (data) {
+async function embed(data) {
 	return {
-		components: [
-			{
-				type: 1,
-				components: [
-					{
-						style: 5,
-						label: `Stream`,
-						url: data.idealLinks.stream,
-						disabled: false,
-						type: 2
-					},
-					{
-						style: 5,
-						label: `Direct Download`,
-						url: data.idealLinks.download,
-						disabled: false,
-						type: 2
-					}
-				]
-			}
-		],
-		embeds: [
-			{
-				type: 'rich',
-				title: `${data.animetitle} - [${data.anime_year}]`,
-				description: `Episode ${data.selectedEpisode} of ${data.total_episodes}`,
-				color: 0xa6b7b7,
-				thumbnail: {
-					url: data.thumbnail,
-					height: 0,
-					width: 0
+		components: [{
+			type: 1,
+			components: [{
+					style: 5,
+					label: `Stream`,
+					url: data.idealLinks.stream,
+					disabled: false,
+					type: 2
 				},
-				fields: [
-					{
-						name: 'Summary',
-						value: data.anime_summary,
-						inline: true
-					}
-				],
-				footer: {
-					text: `Results Obtained From Anime Mate API`,
-					icon_url: `https://animevibe.se/192x192.png`
+				{
+					style: 5,
+					label: `Direct Download`,
+					url: data.idealLinks.download,
+					disabled: false,
+					type: 2
 				}
+			]
+		}],
+		embeds: [{
+			type: 'rich',
+			title: `${data.animetitle} - [${data.anime_year}]`,
+			description: `Episode ${data.selectedEpisode} of ${data.total_episodes}`,
+			color: 0xa6b7b7,
+			thumbnail: {
+				url: data.thumbnail,
+				height: 0,
+				width: 0
+			},
+			fields: [{
+				name: 'Summary',
+				value: data.anime_summary,
+				inline: true
+			}],
+			footer: {
+				text: `Results Obtained From Anime Mate API`,
+				icon_url: `https://animevibe.se/192x192.png`
 			}
-		]
+		}]
 	}
 }
 
-async function search (query) {
+async function search(query) {
 	try {
-		let response = await fetch (`${APIBase}/search/${encodeURI(query.toLowerCase())}`)
+		let response = await fetch(`${APIBase}/search/${encodeURI(query.toLowerCase())}`)
 		let data = await response.json()
 		data.map(a => a.slug = a.url.split('/anime/')[1])
 		return data
-	} catch (err) { return new Error(err) }
+	} catch (err) {
+		return new Error(err)
+	}
 }
 
-async function streams (slug, episode) {
+async function streams(slug, episode) {
 	try {
 		let response = await fetch(`${APIBase}/episode/${slug}/${episode}`)
 		let data = await response.json()
 		return data
-	} catch (err) { return new Error(err) }
+	} catch (err) {
+		return new Error(err)
+	}
 }
 
-async function information (slug) {
+async function information(slug) {
 	try {
 		let response = await fetch(`${APIBase}${slug}`)
 		let data = await response.json()
 		return data
-	} catch (error) { return new Error(err) }
+	} catch (error) {
+		return new Error(err)
+	}
 }
 
-function SSBDownloadLink (link) {
+function SSBDownloadLink(link) {
 	let chunks = link.split('/e/')
 	return `${chunks[0]}/dl?op=download_orig&id=${chunks[1]}&mode=h`
 }
@@ -101,15 +102,15 @@ export default {
 		let results = await search(parameters.query)
 		let question = `**Please respond with the number of the series you want.**\n\n`
 		let range = results.length
-    results.map((item, index) => {
-      while (index <= results.length) {
-        let outgoingMessage = `**${index + 1}** : \`${item.animetitle}\` \n`
-        question = question += outgoingMessage
-        return item
-      }
-    })
+		results.map((item, index) => {
+			while (index <= results.length) {
+				let outgoingMessage = `**${index + 1}** : \`${item.animetitle}\` \n`
+				question = question += outgoingMessage
+				return item
+			}
+		})
 
-    message.channel.send(question)
+		message.channel.send(question)
 
 		const filter = response => {
 			const selection = parseInt(response.content)
@@ -117,9 +118,14 @@ export default {
 				return true
 			}
 		}
-		
+
 		try {
-			const collection = await message.channel.awaitMessages({ filter, max: 1, time: 15000, errors: ['time'] })
+			const collection = await message.channel.awaitMessages({
+				filter,
+				max: 1,
+				time: 15000,
+				errors: ['time']
+			})
 			const selectedValue = collection.first().content - 1
 
 			let infos = await information(results[selectedValue].url)
@@ -153,7 +159,10 @@ export default {
 
 			let embeds = await embed(selection)
 
-			message.reply({ components: embeds.components, embeds: embeds.embeds })
+			message.reply({
+				components: embeds.components,
+				embeds: embeds.embeds
+			})
 
 		} catch (emptyCollection) {
 			console.log(emptyCollection)
@@ -163,4 +172,3 @@ export default {
 
 	}
 }
-
