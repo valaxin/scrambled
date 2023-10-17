75f18e5de0c92d426e7493cc9fade9109003518d
valaxin <valaxin@local>
2023-10-17 15:46:54 -0300

    yehaw! api starting... express and a few others pulled in to make it happen

---
+++ service/api.js
@@ -0,0 +1,36 @@
+import { Router } from 'express'
+import path from 'path'
+import app from './index.js'
+
+const router = Router()
+
+router.get('/', async (req, res, next) => {
+    res.json({ version: '0.0.1', repository: 'https://github.com/valaxin/scrambed' })
+})
+
+router.post('/interaction/:command', async (req, res, next) => {
+    console.log(req.body)
+    console.log(req.params.command)
+
+    switch (req.params.command) {
+        case 'anime-query-api' :
+            console.log('hello!')
+            break
+        
+        deafult :
+            console.log(`${req.params.command} not found!`)
+            break
+
+    }
+
+    res.sendStatus(200)
+})
+
+router.post('/fetch/:resource', async (req, res, next) => {
+    console.log(req.body)
+    console.log(req.params.resource)
+    res.sendStatus(200)
+})
+
+
+export default router
