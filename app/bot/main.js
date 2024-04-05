9c61d9ae69b1c81c13a0a5966cf90949010df212
MacKenzie <mackenzie.bobbitt@outlook.com>
2024-04-05 19:01:07 -0300

    a whole new world
    Pasted boilerplate code from the discord.js guide. can play ping pong. I like how the events and commands are broken down into the there own files

---
+++ app/bot/main.js
@@ -0,0 +1,51 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits, REST, Routes } = require('discord.js');
+const config = require('../../config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// Iterate over every subfolder within the commands directory
+for (const folder of commandFolders) {
+	
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	
+	// Iterate over the files within each discovered folder
+	for (const file of commandFiles) {
+		
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+
+		console.log({ foldersPath, commandFolders, commandsPath, commandFiles, filePath, command })
+		
+		// Should the keys 'data' and 'execute' be available from default export, assume good!
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(config.keys.wumpus.token);
+
+module.exports = client
--- app/source/utilities/api.js
+++
@@ -1,8 +0,0 @@
-export default async function (query, options) {
-  try {
-    console.log(query, options)
-    return ''
-  } catch (error) {
-    return new Error(error)
-  }
-}
