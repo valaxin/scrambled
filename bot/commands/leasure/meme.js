cae3c4ccbb08c5d4d2f777a43a06a869d166c089
MacKenzie <mackenzie.bobbitt@outlook.com>
2024-04-21 14:05:10 -0300

    on, at, by
    added the chalk library for coloring terminal output, added bot/commands/manifest.json to house ALL the static information for commands available within the bot, updated /meme to make use of this manifest

--- bot/commands/leasure/meme.js
+++ bot/commands/leasure/meme.js
@@ -1,47 +1,94 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const { getSingleRandomImageFromSubTop25 } = require('../../support/snooInteractions.js')
+const {
+  getSingleRandomImageFromSubTop25,
+} = require("../../support/snooInteractions.js");
+const { commands } = require("../../manifest.json");
+const path = require("node:path");
+
+const filename = path.basename(__filename).split(".")[0];
+if (!commands[filename]) {
+  module.exports = new Error("missing information within ./manifest.json");
+}
+
+const sentence = (on, at, by) => {
+  return ``;
+};
+
+const cmd = commands[filename];
+const arg = Object.keys(cmd.arguments);
+
+console.log(arg, cmd);
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("meme")
-    .setDescription("Returns a random meme from reddit")
+    .setName(filename)
+    .setDescription(cmd.description)
     .addStringOption((option) =>
       option
-        .setName("subreddit")
-        .setDescription("Define a subreddit")
-        .setRequired(false)
+        .setName(arg[0])
+        .setDescription(cmd.arguments[arg[0]].description)
+        .setRequired(cmd.arguments[arg[0]].required)
     )
     .addStringOption((option) =>
       option
-        .setName('sorting')
-        .setDescription('What reddit')
-        .setRequired(false)
+        .setName(arg[1])
+        .setDescription(cmd.arguments[arg[1]].description)
+        .setRequired(cmd.arguments[arg[1]].required)
+        .addChoices(
+          {
+            name: cmd.arguments[arg[1]].selection[0],
+            value: cmd.arguments[arg[1]].selection[0],
+          },
+          {
+            name: cmd.arguments[arg[1]].selection[1],
+            value: cmd.arguments[arg[1]].selection[1],
+          },
+          {
+            name: cmd.arguments[arg[1]].selection[2],
+            value: cmd.arguments[arg[1]].selection[2],
+          }
+        )
     ),
   async execute(interaction) {
+    console.log(arg);
+    console.log(cmd.arguments[arg[0]].default);
     try {
       const options = {
-        key: interaction.options.getString("subreddit") || 'funny',
-        sort: interaction.options.getString("sorting") || 'hot',
-        limit: 50
+        slug:
+          interaction.options.getString(arg[0]) ||
+          cmd.arguments[arg[0]].default,
+        sort:
+          interaction.options.getString(arg[1]) ||
+          cmd.arguments[arg[1]].default,
+        limit: 50,
       };
-      const results = await getSingleRandomImageFromSubTop25(options)
+      const results = await getSingleRandomImageFromSubTop25(options);
       const getRandomArbitrary = (min, max) => {
         return Math.floor(Math.random() * (max - min) + min);
-      }
-      const position = getRandomArbitrary(0, results.length - 1)
-      const selected = results[position].data
+      };
+      const position = getRandomArbitrary(0, results.length - 1);
+      const selected = results[position].data;
 
-      console.log(selected)
+      console.log(selected);
+      const domain = "https://reddit.com/";
 
       const embed = new EmbedBuilder()
         .setColor(0xffe135)
         .setTitle(selected.title)
         .setImage(selected.url)
-        .setDescription(`--`)
+        .setDescription(
+          `- Posted: **[r/${options.slug}](${domain}/r/${
+            options.slug
+          })** \n - By: **u/[${selected.author}](${domain}/u/${
+            selected.author
+          })** \n - Uploaded: **${Date(selected.created).toLocaleString({
+            timezone: "EST",
+          })}**`
+        );
 
       await interaction.reply({
         embeds: [embed],
-        ephemeral: true,
+        ephemeral: cmd.ephemeral,
       });
     } catch (error) {
       console.error(error);
--- bot/support/snooInteractions.js
+++ bot/support/snooInteractions.js
@@ -1,27 +1,29 @@
-const axios = require('axios')
-const fs = require('node:fs')
+const axios = require("axios");
+const fs = require("node:fs");
 
 module.exports = {
   getSingleRandomImageFromSubTop25: async (options) => {
-    const address = `https://api.reddit.com/r/${options.key}/${options.sort}.json?limit=${options.limit || 25}`
-    const response = await axios.get(address)
-    const unfiltered = await response.data.data.children.map(post => {
-      if (post.data.is_self !== true && post.data.post_hint === 'image') {
+    const address = `https://api.reddit.com/r/${options.slug}/${
+      options.sort
+    }.json?limit=${options.limit || 25}`;
+    const response = await axios.get(address);
+    const unfiltered = await response.data.data.children.map((post) => {
+      if (post.data.is_self !== true && post.data.post_hint === "image") {
         if (post.data.over_18 === false) {
-          if (post.data.url.includes('i.redd.it')) {
-            return post
+          if (post.data.url.includes("i.redd.it")) {
+            return post;
           }
         }
       }
-      return false
+      return false;
       // If given post is 1. not NSFW, 2. an image & 3. not hosted externally
       // --- return a collection of filtered posts based on this critera.
-    })
+    });
 
-    const posts = unfiltered.filter(item => item != false)
+    const posts = unfiltered.filter((item) => item != false);
     if (posts.length > 0) {
-      return posts
+      return posts;
     }
-    return false
-  }
-}
+    return false;
+  },
+};
