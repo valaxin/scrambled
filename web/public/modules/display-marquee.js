608482ca3d004c0ea76b5d236aeb406718be4057
valaxin <15826604+valaxin@users.noreply.github.com>
2024-09-08 08:40:14 -0300

    cleanup...

--- web/public/modules/display-marquee.js
+++ web/public/modules/display-marquee.js
@@ -2,7 +2,7 @@
 
 export default async function DisplayMarquee(stage, data) {
   const element = document.createElement(`div`)
-  element.classList.add('marquee-item')
+  element.classList.add('marquee')
   const template = `<span>${data.author} : ${data.message}</span>`
   element.innerHTML = template
   stage.appendChild(element)
